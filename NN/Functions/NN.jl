1b817ae8c3005d4159b98bf48d85ba489b84986e
axelbjarkar <axelbjarkar@gmail.com>
2024-01-18 19:17:14 +0000

    NN.jl Virkar núna :D

--- NN/Functions/NN.jl
+++ NN/Functions/NN.jl
@@ -18,11 +18,12 @@ using LinearAlgebra
 using JLD2
 # --------- Struct --------- #
 struct NN
-    model
-    loss_funct
-    opt             # optimizer
+    model::Any
+    lr
 end
+
 # --------- Functions --------- #
+
 function load_MNIST()
     """
     Loading the MNIST dataset.
@@ -30,53 +31,50 @@ function load_MNIST()
     each with 28x28 pixel dimensions.
     X: Grayscale vector, Y: Correct label.
     """
-    X_training, Y_training = MNIST(split = :train)[:] 
+    X_training, Y_training = MNIST(split = :train)[:]
     X_testing, Y_testing = MNIST(split = :test)[:]
     X_training = Flux.flatten(X_training)
     X_testing = Flux.flatten(X_testing)
-    Y_training = Flux.onehotbatch(Y_training,0:9)
-    Y_testing = Flux.onehotbatch(Y_testing,0:9)
-    return X_training,Y_training,X_testing,Y_testing
+    Y_training = Flux.onehotbatch(Y_training, 0:9)
+    Y_testing = Flux.onehotbatch(Y_testing, 0:9)
+    return X_training, Y_training, X_testing, Y_testing
 end
-# function loading_bar(epochs)
-#     """
-#     Function creates a loading bar for the training process
-        
-#     ATTENTION: this feature might slow down the traing process alot!
-#     """
-#     return 1
-# end
+
+function loss_of(model)
+    """
+    For a loss function, we use MSE (mean squared error).
+    """
+
+    loss(X, Y) = Flux.Losses.mse(model(X), Y)
+    return loss
+end
+
 function train(nn::NN, epochs, input_params)
     """
     In: NN struct, iteration(epochs), input_params
     Out: trained model with saved parameters
     """
 
-    # Create loading_bar - If you want to use it
-    # loading_bar = loading_bar(epochs)
-    # #loading_bar = loading_bar(epochs)
+    # Loss
+    loss_fn = loss_of(nn.model)
 
     # Load MNIST data
     X_train, Y_train, _, _ = load_MNIST()
-    
+
     if isfile(input_params)
         loaded_dict = load(input_params)
         Flux.loadparams!(nn.model, loaded_dict["model_params"])
     end
-    
+
     data = [(X_train, Y_train)]
+
     for epoch in 1:epochs
-        Flux.train!(nn.loss_funct, nn.model, data, nn.opt)
+        Flux.train!(loss_fn, Flux.params(nn.model), data, Descent(nn.lr))
     end
+
     println("Training completed")
 end
 
-
-
-
-
-"""
-DEMO - how to use
-myNN = NN(model, loss, opt)
-train(myNNNN, 10, "params.jld2")
-"""
+# DEMO - how to use
+# myNN = NN(model, loss, opt)
+# train(myNN, 10, "params.jld2")
