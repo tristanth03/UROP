a03073fac8c121c28f4faad974a5af413a814f9a
tristanth03 <tristanthordarson@gmail.com>
2024-01-18 09:40:35 +0000

    Files

--- NN/Models/model_5LS2048/model_5LS2048.jl
+++ NN/Models/model_5LS2048/model_5LS2048.jl
@@ -1,39 +1,5 @@
 
-# Assuming that packages are already installed:
-# using Pkg
-# Pkg.add("Images") ...
-
-using Images
-using FileIO
-using MLDatasets
-using Flux
-using ImageShow
-using ImageInTerminal
-using ImageIO
-using ImageMagick
-using LinearAlgebra
-using JLD2
-
 #--------- Functions
-function load_MNIST()
-    """
-    Loading the MNIST dataset.
-    10 classes of digits from 0 to 9,
-    each with 28x28 pixel dimensions.
-    X: Grayscale vector, Y: Correct label.
-    """
-
-    X_training, Y_training = MNIST(split = :train)[:] 
-    X_testing, Y_testing = MNIST(split = :test)[:]
-
-    X_training = Flux.flatten(X_training)
-    X_testing = Flux.flatten(X_testing)
-    Y_training = Flux.onehotbatch(Y_training,0:9)
-    Y_testing = Flux.onehotbatch(Y_testing,0:9)
-
-    return X_training,Y_training,X_testing,Y_testing
-end
-
 function model_5LS2048()
     """
     A 5-layer model using 2048 nodes in the inner layers.
