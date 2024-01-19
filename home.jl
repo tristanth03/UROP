1cf98e58641193d7b2ac60f7d07995c8c6da18ed
axelbjarkar <axelbjarkar@gmail.com>
2024-01-19 22:15:22 +0000

    minor edit

--- home.jl
+++ home.jl
@@ -1,12 +1,8 @@
 """
 Author: Axel Bjarkar Sigurjónsson
 
-home.jl serves as the core module for initializing and training neural network models. 
 It interacts with models.jl to access various neural network architectures and utilizes 
-the structure defined in NN.jl for effective model configuration. 
-
-This file acts as a centralized hub, streamlining the process of setting up and training 
-artificial neural networks while maintaining modularity and extensibility.
+the structure defined in NN.jl.
 """
 
 include("NN.jl")
