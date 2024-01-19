830f29e6a3dccd9a728262af207ad8b974fdb285
axelbjarkar <axelbjarkar@gmail.com>
2024-01-19 12:07:35 +0000

    display loss

--- home.jl
+++ home.jl
@@ -4,7 +4,7 @@ Author: Axel Bjarkar Sigurjónsson
 home.jl serves as the core module for initializing and training neural network models. 
 It interacts with models.jl to access various neural network architectures and utilizes 
 the structure defined in NN.jl for effective model configuration. 
-    
+
 This file acts as a centralized hub, streamlining the process of setting up and training 
 artificial neural networks while maintaining modularity and extensibility.
 """
@@ -26,3 +26,4 @@ elapsed_time = @elapsed begin
 
 end # stopwatch stops
 println("Elapsed time: $elapsed_time seconds")
+println("Loss: ",get_loss(myNN))
